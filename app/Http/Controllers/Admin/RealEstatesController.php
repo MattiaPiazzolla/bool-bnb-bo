0d2776d3d23165f2b2fd72e6dee60a6dc767b440
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-11-01 11:50:57 +0100

    aggiunte rotte

--- app/Http/Controllers/Admin/RealEstatesController.php
+++ app/Http/Controllers/Admin/RealEstatesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\real_estates;
 
@@ -23,7 +23,7 @@ class RealEstatesController extends Controller
     {
         // recupero tutti gli immobili
         $real_estates = real_estates::all();
-        return view('real_estates', compact('real_estates'));
+        return view('RealEstates.index', compact('RealEstates'));
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\RealEstatesController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,9 +20,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+    
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('/real_estates', RealEstatesController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
