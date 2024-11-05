90da24c6cd59049e4845df180ba786bee7daab90
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-11-05 10:37:09 +0100

    fix

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
                 <div class="img-box mb-2">
                     <img src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" alt="asd">
                 </div>
-                <span>User-001</span>
+                <span>{{ Auth::user()->name }}</span>
                 <a href="#">Logout</a>
             </div>
         </div>
