8dc776b8c33587ed39a678991687afff0b89fc8e
GiacomoAccardi <giacomo.accardi.lavoro@gmail.com>
2024-11-06 10:14:51 +0100

    resyling home. fixate rotte, adesso sono protette. Aggiunto nome alla tab del browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Bool-b&b - Office')</title>
 
 
     <!-- Fonts -->
@@ -19,6 +19,7 @@
     <!-- Usando Vite -->
     @vite(['resources/js/app.js'])
 </head>
+
 
 <body>
     <div id="app">
